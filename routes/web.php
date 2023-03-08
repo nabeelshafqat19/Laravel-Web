4c10d53a715b13a1e58f70224df6957b78e876c5
Talha Naveed <talhanaveed521@gmail.com>
2023-03-08 20:28:47 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
+
+Route::get('/',[App\Http\Controllers\Frontend\FrontendController::class, 'index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
@@ -30,6 +32,9 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function () {
         Route::get('sliders', 'index');
         Route::get('sliders/create', 'create');
         Route::post('sliders/create', 'store');
+        Route::get('sliders/{slider}/edit', 'edit');
+        Route::put('sliders/{slider}', 'update');
+        Route::get('sliders/{slider}/delete','destroy');
         });
 
 
