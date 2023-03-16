a9416d5d5e18fc947e70cfb2250c833f25ec71ed
Talha Naveed <talhanaveed521@gmail.com>
2023-03-16 20:09:45 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,9 @@ Route::get('/collections', [App\Http\Controllers\Frontend\FrontendController::cl
 Route::get('/collections/{category_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'products']);
 Route::get('/collections/{category_slug}/{product_slug}', [App\Http\Controllers\Frontend\FrontendController::class, 'productView']);
 
-// Route::get('wishlist',);
+Route::middleware(['auth'])->group(function () {
+    Route::get('wishlist', [App\Http\Controllers\Frontend\WishlistController::class, 'index']);
+});
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
