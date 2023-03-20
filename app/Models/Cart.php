c5dbc1a26c6a5a4b04a1279cc1eb49309cacc565
Talha Naveed <talhanaveed521@gmail.com>
2023-03-20 20:57:28 +0500

    update

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,7 +2,10 @@
 
 namespace App\Models;
 
+use App\Models\Product;
+use App\Models\ProductColor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
@@ -17,4 +20,19 @@ class Cart extends Model
         'product_color_id',
         'quantity'
     ];
+
+    /**
+     * Get the user that owns the Cart
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    public function productColor(): BelongsTo
+    {
+        return $this->belongsTo(ProductColor::class, 'product_color_id', 'id');
+    }
 }
