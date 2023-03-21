ba72b3a7f636a8862e6731c84289f907aa99606d
Talha Naveed <talhanaveed521@gmail.com>
2023-03-21 16:41:50 +0500

    changes

---
+++ app/Http/Livewire/Frontend/Checkout/CheckoutShow.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Livewire\Frontend\Checkout;
+
+use App\Models\Cart;
+use Livewire\Component;
+
+class CheckoutShow extends Component
+{
+
+    public $carts, $totalProductAmount = 0;
+
+    public $fullname, $email, $phone, $pincode, $address;
+
+    public function rules()
+    {
+        return [
+            'fullname' => 'required|string|max:121',
+            'email' => 'required|email|max:121',
+            'phone' => 'required|integer|max:11|min:10',
+            'pincode' => 'required|integer|max:5|max:5',
+            'address' => 'required|string|max:500',
+        ];
+    }
+
+    public function codOrder()
+    {
+        # code...
+    }
+
+    public function totalProductAmount()
+    {
+        $this->carts = Cart::where('user_id', auth()->user()->id)->get();
+        foreach ($this->carts as $cartItem) {
+            $this->totalProductAmount += $cartItem->product->selling_price * $cartItem->quantity;
+        }
+        return $this->totalProductAmount;
+    }
+
+    public function render()
+    {
+        $this->totalProductAmount = $this->totalProductAmount();
+        return view('livewire.frontend.checkout.checkout-show', [
+            'totalProductAmount' => $this->totalProductAmount
+        ]);
+    }
+}
