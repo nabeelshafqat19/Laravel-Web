036386b9e8a4e1902b98b54072d852ec8cf4d36a
usman murtaza <usmanmurtaza729@gmail.com>
2023-03-22 22:44:32 +0500

    change

--- resources/views/livewire/frontend/cart/cart-show.blade.php
+++ resources/views/livewire/frontend/cart/cart-show.blade.php
@@ -104,7 +104,11 @@
                             <span class="float-end">Rs{{ $totalPrice }}</span>
                         </h4>
                         <hr>
+<<<<<<< Updated upstream
                         <a href="{{ url('/checkout') }}" class="btn btn-warning w-100">Checkout</a>
+=======
+                        <a href="{{ url('/checkout'} }" class="btn btn-warning w-100">Checkout</a>
+>>>>>>> Stashed changes
                     </div>
                 </div>
             </div>
