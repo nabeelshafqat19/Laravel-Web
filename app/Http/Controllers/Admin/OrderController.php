84744a0b6a65fcb19552c8617416d80b7e643b11
Talha Naveed <talhanaveed521@gmail.com>
2023-03-24 22:58:07 +0500

    update

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -6,6 +6,7 @@ use App\Models\Order;
 use Illuminate\Support\Carbon;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class OrderController extends Controller
 {
@@ -40,5 +41,36 @@ class OrderController extends Controller
         }else{
             return redirect('admin/orders')->with('message','Order Id is not found');
         }
+    }
+
+    public function updateOrderStatus(int $orderId, Request $request)
+    {
+        $order = Order::where('id',$orderId)->first();
+        if($order){
+
+            $order->update([
+                'status_message' => $request->order_status
+            ]);
+            return redirect('admin/orders/'.$orderId)->with('message','Order Status Updated');
+        }else{
+            return redirect('admin/orders/'.$orderId)->with('message','Order Id is not found');
+        }
+    }
+
+    public function viewInvoice(int $orderId)
+    {
+        $order = Order::findOrFail($orderId);
+        return view('admin.invoice.generate-invoice', compact('order'));
+    }
+
+    public function generateInvoice(int $orderId)
+    {
+        $order = Order::findOrFail($orderId);
+        $data = ['order' => $order];
+
+        $pdf = Pdf::loadView('admin.invoice.generate-invoice', $data);
+
+        $todayDate = Carbon::now()->format('d-m-Y');
+        return $pdf->download('invoice'.$order->id.'-'.$todayDate.'.pdf');
     }
 }
